f1283e4b1e865d2b46a9fb6d6ecbf8ad288d0d5a
chaeyoung103 <thdcodud103@naver.com>
2025-08-08 17:03:39 +0900

    feat: globalexceptionhandler 추가

---
+++ src/main/java/com/example/hanaro/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.example.hanaro.global.exception;
+
+import static com.example.hanaro.global.response.BaseErrorCode.*;
+
+import com.example.hanaro.global.response.BaseErrorResponse;
+import com.example.hanaro.global.response.ErrorCode;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public BaseErrorResponse handleValidationException(MethodArgumentNotValidException e) {
+		log.error("[handleValidationException]", e);
+		String errorMessage = e.getBindingResult().getFieldErrors().stream()
+			.findFirst()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.orElse("잘못된 요청입니다.");
+
+		return new BaseErrorResponse(new ErrorCode() {
+			@Override
+			public int getStatus() {
+				return HttpStatus.BAD_REQUEST.value();
+			}
+
+			@Override
+			public String getCode() {
+				return "B003";
+			}
+
+			@Override
+			public String getMessage() {
+				return errorMessage;
+			}
+		});
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public BaseErrorResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+		log.error("[handleMethodArgumentTypeMismatchException]", e);
+		return new BaseErrorResponse(ARGUMENT_TYPE_MISMATCH);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MissingServletRequestPartException.class)
+	public BaseErrorResponse handleMissingServletRequestPartException(MissingServletRequestPartException e) {
+		log.error("[handleMissingServletRequestPartException]", e);
+		return new BaseErrorResponse(IMAGE_NOT_PRESENT);
+	}
+
+}
