4ebcc8b31fb6e77cf428077801c11c165050466e
chaeyoung103 <thdcodud103@naver.com>
2025-08-08 17:03:23 +0900

    feat: 스케쥴링을 위한 어노테이션 추가

--- src/main/java/com/example/hanaro/HanaroApplication.java
+++ src/main/java/com/example/hanaro/HanaroApplication.java
@@ -3,8 +3,10 @@ package com.example.hanaro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HanaroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HanaroApplication.class, args);
