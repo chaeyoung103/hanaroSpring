663ff10fef3351de3854b2d68a0cf7d8cbfd0a0e
chaeyoung103 <thdcodud103@naver.com>
2025-08-08 14:35:56 +0900

    feat: baseexception 추가

---
+++ src/main/java/com/example/hanaro/global/exception/BaseException.java
@@ -0,0 +1,18 @@
+package com.example.hanaro.global.exception;
+
+import lombok.Getter;
+
+// @Getter
+// public class BaseException extends RuntimeException {
+// 	private final ErrorCode exceptionStatus;
+//
+// 	public BaseException(ErrorCode exceptionStatus) {
+// 		super(exceptionStatus.getMessage());
+// 		this.exceptionStatus = exceptionStatus;
+// 	}
+//
+// 	public BaseException(ErrorCode exceptionStatus, String customMessage) {
+// 		super(exceptionStatus.getMessage() + " (" + customMessage + ")");
+// 		this.exceptionStatus = exceptionStatus;
+// 	}
+// }
